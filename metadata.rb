ccc4e3e24a001857194a9716a97da768df80e98b
Tomoya Kabe <limit.usus@gmail.com>
2018-03-13 09:35:40 +0900

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'kabe@elastic-infra.com'
 license 'MIT'
 description 'Installs/Configures chrony_ii'
 long_description 'Installs/Configures chrony_ii'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/elastic-infra/chrony_ii/issues'
