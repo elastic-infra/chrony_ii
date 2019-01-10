e4614515884a134502593e4fcbc8bd806919a27a
Bob Sutterfield <bsutterfield@ellation.com>
2019-01-10 13:19:21 -0800

    Reduce lines

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -26,50 +26,42 @@
 
 require 'spec_helper'
 
+# for a complete list of available platforms and versions see:
+# https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
+
 describe 'chrony_ii::default' do
   context 'When all attributes are default, on Ubuntu 16.04' do
     let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
       runner = ChefSpec::SoloRunner.new(
         platform: 'ubuntu',
         version: '16.04'
       )
       runner.converge(described_recipe)
     end
-
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
   end
-
   context 'When all attributes are default, on Ubuntu 14.04' do
     let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
       runner = ChefSpec::SoloRunner.new(
         platform: 'ubuntu',
         version: '14.04'
       )
       runner.converge(described_recipe)
     end
-
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
   end
-
   context 'When all attributes are default, on CentOS 7.4.1708' do
     let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
       runner = ChefSpec::SoloRunner.new(
         platform: 'centos',
         version: '7.4.1708'
       )
       runner.converge(described_recipe)
     end
-
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
