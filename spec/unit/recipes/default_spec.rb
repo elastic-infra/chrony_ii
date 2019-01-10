36c9c934bfee419ddc91cbd6f7baae6ffc34361b
Bob Sutterfield <bsutterfield@ellation.com>
2019-01-10 12:58:24 -0800

    Support CI on ubuntu-14.04

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -43,6 +43,23 @@ describe 'chrony_ii::default' do
     end
   end
 
+  context 'When all attributes are default, on Ubuntu 14.04' do
+    let(:chef_run) do
+      # for a complete list of available platforms and versions see:
+      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
+      runner = ChefSpec::SoloRunner.new(
+        platform: 'ubuntu',
+        version: '14.04'
+      )
+      runner.converge(described_recipe)
+    end
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+  end
+
+
   context 'When all attributes are default, on CentOS 7.4.1708' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
