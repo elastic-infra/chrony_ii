fb1a292d302ffc9ad7dbf114be83b5171c7b111d
Tomoya Kabe <limit.usus@gmail.com>
2019-03-07 01:36:04 +0900

    When using Amazon Time Sync Service, use only it

--- test/smoke/amazon_time/config_test.rb
+++ test/smoke/amazon_time/config_test.rb
@@ -11,4 +11,6 @@ describe file(config_path) do
   its('mode') { should eq 0o644 }
   its('content') { should match(/^# Chef managed/) }
   its('content') { should match(/^server 169\.254\.169\.123 prefer iburst$/) }
+  its('content') { should match(/^initstepslew 30 169\.254\.169\.123$/) }
+  its('content') { should_not match(/^pool/) }
 end
